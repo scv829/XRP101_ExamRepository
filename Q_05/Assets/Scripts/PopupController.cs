77eaf82f7707e64e69fcef5a7ae8e9800b812632
scv829 <scv829@naver.com>
2024-11-19 14:19:43 +0900

    Refactor: 문제 5번 코드 수정
    - 게임매니저 싱글톤 로직 수정
    - 게임매니저 게임 재개 추가
    - 큐브 회전 로직 수정
    - 팝업 대기 코루틴 수정

--- Q_05/Assets/Scripts/PopupController.cs
+++ Q_05/Assets/Scripts/PopupController.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class PopupController : MonoBehaviour
 {
     [SerializeField] private float _deactiveTime;
-    private WaitForSeconds _wait;
+    // private WaitForSeconds _wait;
+    private WaitForSecondsRealtime _wait;
     private Button _popupButton;
 
     [SerializeField] private GameObject _popup;
@@ -18,7 +19,8 @@ public class PopupController : MonoBehaviour
 
     private void Init()
     {
-        _wait = new WaitForSeconds(_deactiveTime);
+        //_wait = new WaitForSeconds(_deactiveTime);
+        _wait = new WaitForSecondsRealtime(_deactiveTime);
         _popupButton = GetComponent<Button>();
         SubscribeEvent();
     }
@@ -28,21 +30,22 @@ public class PopupController : MonoBehaviour
         _popupButton.onClick.AddListener(Activate);
     }
 
-    private void Activate()
+    private void Activate() // 팝업창 보이는 로직
     {
-        _popup.gameObject.SetActive(true);
-        GameManager.Intance.Pause();
-        StartCoroutine(DeactivateRoutine());
+        _popup.gameObject.SetActive(true);      // 1. 팝업창 보이기
+        GameManager.Intance.Pause();            // 2. 게임 스케일 0으로 조절
+        StartCoroutine(DeactivateRoutine());    // 3. 루틴 시작
     }
 
     private void Deactivate()
     {
-        _popup.gameObject.SetActive(false);
+        GameManager.Intance.Resume();          // 추가. 게임 재개
+        _popup.gameObject.SetActive(false);     // 6. 비활성화
     }
 
     private IEnumerator DeactivateRoutine()
     {
-        yield return _wait;
-        Deactivate();
+        yield return _wait; // 4. 대기 시간이 지나고
+        Deactivate();       // 5. 비활성화 함수 호출
     }
 }
