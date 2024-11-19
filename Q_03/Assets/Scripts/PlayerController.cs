f97addcf48e84fd616b5a1ed662bfb2a17b86832
scv829 <scv829@naver.com>
2024-11-19 12:24:40 +0900

    Refactor: 문제 3번 코드 수정
    - bullet 발사 속도 로직 변경
    - 플레이어 비활성화 방법 수정
    - 타워 공격 범위 이탈 로직 추가

--- Q_03/Assets/Scripts/PlayerController.cs
+++ Q_03/Assets/Scripts/PlayerController.cs
@@ -30,9 +30,26 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // 게임 오브젝트가 비활성화 되면 효과음 자체가 안나온다
+    // 1. 효과음이 다 나오고 게임 오브젝트 비활성화 <- 코루틴으로 구현
+    // 근데 게임 오브젝트 비활성호 + 효과음 재생이 하나여서 해당 방법은 부적절함
+    // IEnumerator DieCoroutine()
+    // {
+    //     _audio.Play();
+    //     float time = 0;
+    //     while (time < _audio.clip.length)
+    //     {
+    //         time += Time.deltaTime;
+    //         yield return null;
+    //     }
+    //     gameObject.SetActive(false);
+    // }
+
+    // 2. 충돌체는 body가 가지고 있기 때문에 body만 비활성화
     public void Die()
     {
         _audio.Play();
-        gameObject.SetActive(false);
+        transform.GetChild(0).gameObject.SetActive(false);
     }
+
 }
