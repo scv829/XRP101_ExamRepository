fb278ee0a2800c171bee81bae09535d26d757954
scv829 <scv829@naver.com>
2024-11-19 14:44:20 +0900

    Refactor: 문제 6번 코드 수정
    - 카메라 위치 설정 로직 수정
    - Gun 스크립트의 레이캐스트 방향 수정
    - Gun 스크립트의 레이어 자동 할당 추가

--- Q_06/Assets/Scripts/CameraController.cs
+++ Q_06/Assets/Scripts/CameraController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool _hasFollowTarget;
-    private Transform _followTarget;
+    [SerializeField] private bool _hasFollowTarget;
+    [SerializeField] private Transform _followTarget;
     public Transform FollowTarget
     {
         get => _followTarget;
@@ -24,9 +24,12 @@ public class CameraController : MonoBehaviour
     {
         if (!_hasFollowTarget) return;
 
-        _followTarget.SetPositionAndRotation(
-            transform.position,
-            transform.rotation
-            );
+        // 1. 카메라 위치 및 회전이 적용안됨
+        //_followTarget.SetPositionAndRotation(
+        //    transform.position,
+        //    transform.rotation
+        //    );
+        /// 타겟이 카메라를 따라오게 되어있어서 카메라가 타겟을 따라가게로 변경
+        transform.SetPositionAndRotation(_followTarget.position, _followTarget.rotation);
     }
 }
